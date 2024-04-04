19336052747b620db1deec6f05b85e23d12183de
seller <galiev.rasul.2002@yandex.ru>
2024-04-04 10:45:46 +0300

    create components notes-tab

---
+++ src/app/components/notes-tab/notes-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './notes-tab.component.html',
+  styleUrl: './notes-tab.component.css'
+})
+export class NotesTabComponent {
+
+}
