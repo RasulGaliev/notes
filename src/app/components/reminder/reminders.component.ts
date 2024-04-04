8c4cdfd076568751b73a97ac0288c73891ed74df
seller <galiev.rasul.2002@yandex.ru>
2024-04-04 10:47:19 +0300

    rename reminders component

--- src/app/components/reminder/reminders.component.ts
+++ src/app/components/reminder/reminders.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-reminders',
+  selector: 'app-reminder',
   standalone: true,
   imports: [],
   templateUrl: './reminders.component.html',
