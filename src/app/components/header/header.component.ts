a35a30045856cbb2d63a7570c121fb260ca0c015
seller <galiev.rasul.2002@yandex.ru>
2024-04-04 14:46:36 +0300

    crete header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  logoUrl: string = './assets/images/logo/logo.svg';
 
+  mainPageUrl: string = '';
+  notesPageUrl: string = 'notes';
+  tagsUrl: string = 'tags';
+  remindersPageUrl: string = 'reminders';
 }
