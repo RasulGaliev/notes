d6e8c65ec12bec3539bc4f3b6fbd14ec40eaf4a5
seller <galiev.rasul.2002@yandex.ru>
2024-04-04 10:46:04 +0300

    create components reminders-tab

---
+++ src/app/components/reminders-tab/reminders-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reminders-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './reminders-tab.component.html',
+  styleUrl: './reminders-tab.component.css'
+})
+export class RemindersTabComponent {
+
+}
