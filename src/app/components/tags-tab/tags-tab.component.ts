a8060b2d4cb47490555a486949de004881229674
seller <galiev.rasul.2002@yandex.ru>
2024-04-04 10:45:16 +0300

    create components tags-tab, reminders-tab and notes-tab

---
+++ src/app/components/tags-tab/tags-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tags-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './tags-tab.component.html',
+  styleUrl: './tags-tab.component.css'
+})
+export class TagsTabComponent {
+
+}
